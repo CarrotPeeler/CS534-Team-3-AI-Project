342000a46f659737ccb3d1b4ac25a4fa4728481c
CarrotPeeler <jared24mc@gmail.com>
2023-10-24 01:47:18 -0400

    added script for yolo inference/bbox extraction

--- README.md
+++ README.md
@@ -1,20 +1,21 @@
 # CS534-Team-3-AI-Project
 Something something here...
 
-### Dependencies
+## Dependencies
 - idk
 
-### Datasets Used
+## Datasets Used
 Download the following datasets:
 - [Augmented PlantVillage Dataset for Image Classification](https://www.kaggle.com/datasets/vipoooool/new-plant-diseases-dataset)
   - NOTE: PlantVillage uses plant images taken in a labratory, unlike PlantDoc which uses photos of plants out in nature; PlantVillage provides more data overall, however
 - [PlantDoc Datset for Image Classification](https://github.com/pratikkayal/PlantDoc-Dataset.git)
 - [PlantDoc Datset for Object Detection](https://universe.roboflow.com/joseph-nelson/plantdoc)
 
+## Running YOLOv8
 ### Data Preprocessing
-Run YOLOv8 inference over PlantDoc Object Detection test split:
+Reformat annotations to only reflect species and not disease
 ```
-python3  
+python3 preprocessing/downscale_annotations.py
 ```
 
 ### Training
@@ -26,4 +27,7 @@ python3 scripts/train_net.py < /dev/null > train_log.txt 2>&1 &
 ```
 
 ### Inference
+
+
+## Running Fine-grained Classification
 
--- preprocessing/YOLOv8/scripts/extract_bbox_anno.py
+++ preprocessing/YOLOv8/scripts/extract_bbox_anno.py
@@ -1,4 +1,5 @@
 import os
+import pandas as pd
 from preprocessing.YOLOv8.ultralytics.models import YOLO
 
 if __name__ == "__main__":
@@ -10,6 +11,56 @@ if __name__ == "__main__":
     # data path
     data_dir = f"{root}/PlantDoc_ObjectDetection/test"
 
-    for img_anno_file in os.listdirdata_dir 
+    # get all annotation file names without the .txt extension
+    raw_anno_names = list(map(lambda x:x.rpartition('.')[0], os.listdir(data_dir)))
 
-    results = model.predict(source=data_yaml_path, imgsz=640, save_conf=True, save_txt=True)
+    # columns
+    img = []
+    target_cls = []
+    target_xywh = []
+    bbox_xyxy = []
+    bbox_conf = []
+    top1 = []
+    top5 = []
+    top1_conf = []
+    top5_conf = []
+
+    for img_name in raw_anno_names:
+        # get path to img
+        anno_path = data_dir + '/' + img_name + '.txt'
+        img_path = data_dir + '/' + img_name + '.jpg'
+
+        # grab target class and bbox xywh
+        with open(anno_path) as f:
+            labels = f.readlines()
+            for label in labels:    
+                cls, x, y, w, h = f.read().split()
+                target_cls.append(int(cls))
+                target_xywh.append()
+
+        result = model.predict(source=img_path, imgsz=640)
+        for r in result:
+            boxes = r.boxes.cpu()
+            probs = r.probs.cpu()
+        
+        # append results
+        img.append(img_name)
+        bbox_xyxy.append(boxes.xywh)
+        bbox_conf.append(boxes.conf)
+        top1.append(probs.top1)
+        top5.append(probs.top5)
+        top1_conf.append(probs.top1conf)
+        top5_conf.append(probs.top5conf)
+
+    results = pd.DataFrame()
+    results["image"] = img
+    results["bbox_xywh"] = bbox_xyxy
+    results["bbox_conf"] = bbox_conf
+    results["top1"] = top1
+    results["top5"] = top5
+    results["top1_conf"] = top1_conf
+    results["top5_conf"] = top5_conf
+    results.to_csv(f"{root}/yolo_results.csv", index=False)
+    
+
+        
